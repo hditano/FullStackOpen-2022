71ea33565c6e26b37b78644672546fd12cd69382
Hernan Di Tano <hditano@gmail.com>
2022-08-05 02:18:42 +0200

    added phonebook 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,42 +3,61 @@ import { useState } from 'react'
 const App = () => {
   const [persons, setPersons] = useState([
     { 
-      name: 'Arto Hellas' 
+      name: 'Arto Hellas',
+      number: '111-222-333'
     }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-const handleChange = (e) => {
+
+const handleChangeName = (e) => {
   e.preventDefault();
   setNewName(e.target.value)
 }
 
+const handleChangeNumber = (e) => {
+  e.preventDefault();
+  setNewNumber(e.target.value)
+}
+
 const submitNote = (e) => {
 
+  e.preventDefault();
+
   for(let i = 0; i < persons.length; i++) {
-    if(persons[i].name === newName){
-
-      alert(`${newName} is already added to phonebook`)
-
-    } else {
-
-      e.preventDefault();
-      const newNote = {
-        name: newName
-      }
     
+    const equalityName = (ele) => ele.name === newName;
+
+    const newNote = {
+      name: newName,
+      number: newNumber
+    }
+
+
+    if(persons.some(equalityName)) {
+      alert(`${newName} is already added to phonebook`)
+      setNewName('');
+      setNewNumber('');
+      return;
+    }
+
       setPersons(persons.concat(newNote))
       setNewName('');
-    } 
+      setNewNumber('');
+
+    }
   }
-}
+
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={submitNote}>
         <div>
-          name: <input value={newName} onChange={handleChange}/>
+          name: <input value={newName} onChange={handleChangeName}/>
+          <br></br>
+          number: <input value={newNumber} onChange={handleChangeNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -49,12 +68,13 @@ const submitNote = (e) => {
       {persons.map((ele) => {
         return (
           <>
-            <p key={ele.name}>{ele.name}</p>
+            <p key={ele.name}>{ele.name} {ele.number}</p>
           </>
         )
       })}
     </div>
   )
 }
+  
 
 export default App
