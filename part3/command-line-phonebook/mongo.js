d9539a503362df8b82371f4378b72ca6c601dcf4
Hernan Di Tano <hditano@gmail.com>
2022-08-09 14:48:53 +0200

    added command-line-phonebook 3.12 partially

---
+++ part3/command-line-phonebook/mongo.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+if(process.argv.length < 4) {
+    console.log('add last argv as expected (entry to create');
+    process.exit(1);
+}
+
+const password = process.argv[2];
+const URL = `mongodb+srv://fullstackopen:${password}@cluster0.txqggwq.mongodb.net/PhonebookApp?retryWrites=true&w=majority`;
+
+
+const PhonebookSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const person = mongoose.model('phonebook', PhonebookSchema);
+
+
+
+mongoose
+    .connect(URL)
+    .then((result) => {
+        console.log('connected');
+
+        const myPhonebook = new person({
+            name: process.argv[3],
+            number: process.argv[4]
+        })
+
+        return myPhonebook.save()
+    })
+    .then((result) => {
+        console.log(`Added ${process.argv[3]} number ${process.argv[4]} to phonebook`);
+        return mongoose.connection.close();
+    })
