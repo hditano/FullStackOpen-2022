4130bb78d7531f1e78218ebe0b22913c42877387
Hernan Di Tano <hditano@gmail.com>
2022-08-09 20:39:41 +0200

    added updated files for phonebook database

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
+app.use(express.static('build'));
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
 morgan.token('body', res => {
