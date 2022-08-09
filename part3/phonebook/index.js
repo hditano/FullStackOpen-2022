54b11f6acb9e5090f55686c8a0bb642ba6dd808f
Hernan Di Tano <hditano@gmail.com>
2022-08-09 04:27:19 +0200

    add heroku settins

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
+
+
 
 app.use(express.json());
-
+app.use(cors());
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
 morgan.token('body', res => {
@@ -107,9 +110,9 @@ const generateID = () => {
     return maxID + 1; 
 }
 
-const PORT = 3001;
 
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
 })
