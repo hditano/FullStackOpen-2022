85efa27397ab7b8db22e1462b32c06c7db78bc23
Hernan Di Tano <hditano@gmail.com>
2022-08-12 03:42:57 +0200

    added phonebook 4.2

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,12 +1,60 @@
 const express = require('express');
-const blogSchema = require('../models/blog');
+const BlogSchema = require('../models/blog');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('create blog');
+
+
+router.get('/blog', (req, res) => {
+    BlogSchema
+        .find({})
+        .then((data) => {
+            res.json(data)
+        })
+        .catch(error => console.log(`Error: ${error.message}`))
 })
 
-module.exports = {
-    router
-};
+router.get('/blog/:id', (req, res) => {
+    BlogSchema
+        .findById(req.params.id)
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch(error => console.log(`Error: ${error.message}`))
+})
+
+router.post('/blog', (req, res) => {
+    const newBlog = BlogSchema(req.body);
+
+    newBlog
+        .save()
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch(error => console.log(`Error: ${error.message}`))
+        
+    })
+
+router.delete('/blog/:id', (req, res) => {
+    BlogSchema
+        .findByIdAndRemove(req.params.id)
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch(error => console.log(`Error: ${error.message}`));
+})
+
+router.put('/blog/:id', (req, res) => {
+    const {title, author, url, likes} = req.body
+
+    BlogSchema
+        .findByIdAndUpdate(req.params.id, {title, author, url, likes }, {new: true})
+        .then((data) => {
+            res.status(200).json(data)
+        })
+        .catch(error => console.log(`Error: ${error.message}`))
+})
+
+
+
+module.exports = router;
