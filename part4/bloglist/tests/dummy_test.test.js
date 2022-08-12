1510cf39673098817253caefa1234ecf99696c54
Hernan Di Tano <hditano@gmail.com>
2022-08-12 16:15:03 +0200

    added bloglist 4.4

--- part4/bloglist/tests/dummy_test.test.js
+++ part4/bloglist/tests/dummy_test.test.js
@@ -1,8 +1,29 @@
 const listHelper = require('../utils/list_helper');
 
 test('dummy returns one', () => {
-  const blogs = [1]
+    const blogs = [1]
 
-  const result = listHelper.dummy(blogs)
-  expect(result).toBe(1)
+    const result = listHelper.dummy(blogs)
+    expect(result).toBe(1)
+})
+
+
+describe('total likes', () => {
+
+    const listWithOneBlog = [{
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    }]
+
+
+
+    test('total sum of all likes', () => {
+
+        const result = listHelper.totalLikes(listWithOneBlog);
+        expect(result).toBe(5);
+    })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return blogs.length;
   }
   
+const totalLikes = (blogs) => {
+    return blogs.map(ele => ele.likes)
+                .reduce((accValue, currValue) =>  {
+                    accValue + currValue});
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
