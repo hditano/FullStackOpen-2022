005bded9cd94a666dec5f383e7eed3ec797777ed
Hernan Di Tano <hditano@gmail.com>
2022-08-13 05:17:42 +0200

    added bloglist 4.11

--- part4/bloglist/tests/Server_Test.test.js
+++ part4/bloglist/tests/Server_Test.test.js
@@ -16,9 +16,25 @@ describe('HTTP GET API Request Test', () =>    {
     test('expect returns on how many items are on the API', async () => {
         const response = await api.get('/api/blog');
 
-        expect(response.body).toHaveLength(7);
+
+        expect(response.body).toHaveLength(response.length);
     })
 
+})
+
+describe('HTTP POST API Request Tests', () => {
+
+    test('Make a post successfully', async () => {
+        const response = await api.post('/api/blog').send({title: 'Star Wars', author: 'Hernan', url: 'www.google.com', likes: 22});
+
+        expect(response.statusCode).toBe(201)
+    })
+
+    test('Verifies if likes property is missing', async () => {
+        const res =  await api.post('/api/blog').send({title: 'Star Wars', author: 'Hernan', url: 'www.google.com'});
+
+        expect(res.body).not.toHaveProperty('likes')
+    })
 })
 
 
