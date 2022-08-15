52beeb33216019e57251ac33f7a11791c0292272
Hernan Di Tano <hditano@gmail.com>
2022-08-15 15:57:18 +0200

    added bloglist 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const BlogSchema = require('../models/blog');
 const userSchema = require('../models/user');
+const jwt = require('jsonwebtoken');
 
 
 const getBlog =  async (req, res) => {
@@ -17,11 +18,26 @@ const getBlogID = async (req, res) => {
         
 };
 
+const getToken = request => {
+        const authorization = request.get('authorization');
+        if( authorization && authorization.toLowerCase().startsWith('bearer ')){
+                return authorization.substring(7)
+        }
+        return null;
+}
+
+
 const postBlog = async (req, res) => {
     
         const {title, author, url, likes, userId} = req.body;
+        const token = getToken(req);
 
-        const user = await userSchema.findById(userId);
+        const decodedToken = jwt.verify(token, process.env.SECRET);
+        if(!decodedToken.id) {
+                return res.status(400).json({error: 'token missing or invalid'});
+        }
+
+        const user = await userSchema.findById(decodedToken.id)
 
         const newBlog = new BlogSchema({
             title,
