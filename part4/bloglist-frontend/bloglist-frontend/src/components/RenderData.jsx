50edb4858f445247d415f0f747a79eee059b5f17
Hernan Di Tano <hditano@gmail.com>
2022-08-20 14:02:21 +0200

    added bloglist-frontend 5.1 final

--- part4/bloglist-frontend/bloglist-frontend/src/components/RenderData.jsx
+++ part4/bloglist-frontend/bloglist-frontend/src/components/RenderData.jsx
@@ -3,11 +3,11 @@ const RenderData = ({data}) => {
     // console.log(Object.entries(data).map(ele => ele[1]))
  
   console.log(data)
-  const render = Object.entries(data).map(ele => <p>{ele[1].title}</p>)
+  const render = Object.entries(data).map(ele => <p>Title:{ele[1].title} - Author: {ele[1].author}</p>)
 
   return (
     <div>
-      <p>Blogs saved</p>
+      <h2>Blogs saved</h2>
       {render}
     </div>
   )
