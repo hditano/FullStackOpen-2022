f3d16a29689783cd10681dbe979c8535ccea400e
Hernan Di Tano <hditano@gmail.com>
2022-08-24 20:49:39 +0200

    added bloglist-frontend 5.19

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -42,4 +42,34 @@ describe('Blog App', () => {
         expect(res.status).equal(400)
       })
     })
-  })
+})
+describe('when logged in', () => {
+      beforeEach(() => {
+      const login = {
+        name: 'hditano',
+        password: '456'
+      };
+    cy.contains('Login');
+    cy.get('#loginUsername').type(login.name);
+    cy.get('#loginPassword').type(login.password);
+    cy.get('#loginButton').click();
+
+    cy.contains(`Welcome ${login.name}`)
+    });
+  it('A blog can be created', () => {
+      const newBlog = {
+        title: 'My Test',
+        author: 'Hernan',
+        url: 'www.google.com',
+        likes: 10,
+      }
+    cy.contains('Create').click();
+    cy.get('[name=title]').type(newBlog.title);
+    cy.get('[name=author]').type(newBlog.author);
+    cy.get('[name=url]').type(newBlog.url);
+    cy.get('#button').click();
+        })
+    })
+
+
+
