9ef858925bad711038abbdce2b94914a1b145077
Hernan Di Tano <hditano@gmail.com>
2022-08-24 14:43:52 +0200

    added bloglist-frontend 5.18

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,6 +1,14 @@
 describe('Blog App', () => {
+  
+  const failedLogin = {
+    name: "hditano",
+    password: "446"
+  }
+
   beforeEach(() => {
+
     cy.visit('http://localhost:3000');
+
   })
   it('Logs in correctly', () => {
     cy.contains('Login');
@@ -10,4 +18,27 @@ describe('Blog App', () => {
 
     cy.contains('Welcome hditano')
       })
+  it('Logs incorrectly', () => {
+    cy.contains('Login');
+    cy.get('#loginUsername').type(failedLogin.name);
+    cy.get('#loginPassword').type(failedLogin.password);
+    cy.get('#loginButton').click();
+
+    cy.contains('Username or Password invalid')
+    })
+  it.skip('Can create user', () => {
+    const user = {
+      username: "zoeci",
+      name: "Zoe",
+      password: "123"
+    }
+
+    cy.request('POST', 'http://localhost:3001/api/user/', user)
+    })
+     it('fails login on BackEnd', () => {
+    cy.request({method: 'POST', url: 'http://localhost:3001/api/login', body: failedLogin, failOnStatusCode: false})
+      .then((res) => {
+        expect(res.status).equal(400)
+      })
+    })
   })
